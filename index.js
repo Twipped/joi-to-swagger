817177e11513ad6cb703e249bf30b378ba594a0c
ChiperSoft <chiper@chipersoft.com>
2017-01-03 15:18:08 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,283 @@
+'use strict';
+
+var joi = require('joi');
+var find = require('lodash.find');
+var get = require('lodash.get');
+
+module.exports = exports = function parse (schema, existingDefinitions) {
+	// inspect(schema);
+
+	if (!schema) throw new Error('No schema was passed.');
+
+	if (typeof schema === 'object' && !schema.isJoi) {
+		schema = joi.object().keys(schema);
+	}
+
+	if (!schema.isJoi) throw new TypeError('Passed schema does not appear to be a joi schema.');
+
+	var metaDefName = meta(schema, 'className');
+
+	// if the schema has a definition class name, and that
+	// definition is already defined, just use that definition
+	if (metaDefName && existingDefinitions && existingDefinitions[metaDefName]) {
+		return { swagger: refDef(metaDefName) };
+	}
+
+	var swagger;
+	var definitions = {};
+
+	if (parseAsType[schema._type]) {
+		swagger = parseAsType[schema._type](schema, existingDefinitions, definitions);
+	} else {
+		throw new TypeError(`${schema._type} is not a recognized Joi type.`);
+	}
+
+	if (schema._valids && schema._valids.has(null)) {
+		swagger.type = [ swagger.type, 'null' ];
+	}
+
+	if (schema._description) {
+		swagger.description = schema._description;
+	}
+
+	var defaultValue = get(schema, '_flags.default');
+	if (defaultValue) {
+		swagger.default = defaultValue;
+	}
+
+	if (metaDefName) {
+		definitions[metaDefName] = swagger;
+		return { swagger: refDef(metaDefName), definitions };
+	}
+
+	var override = meta(schema, 'swagger');
+	if (override) {
+		Object.assign(swagger, override);
+	}
+
+	return { swagger, definitions };
+};
+
+var parseAsType = {
+	number: (schema) => {
+		var swagger = {};
+
+		if (find(schema._tests, { name: 'integer' })) {
+			swagger.type = 'integer';
+		} else {
+			swagger.type = 'number';
+			if (find(schema._tests, { name: 'precision' })) {
+				swagger.format = 'double';
+			} else {
+				swagger.format = 'float';
+			}
+		}
+
+		if (find(schema._tests, { name: 'positive' })) {
+			swagger.minimum = 1;
+		}
+
+		if (find(schema._tests, { name: 'negative' })) {
+			swagger.maximum = -1;
+		}
+
+		var min = find(schema._tests, { name: 'min' });
+		if (min) {
+			swagger.minimum = min.arg;
+		}
+
+		var max = find(schema._tests, { name: 'max' });
+		if (max) {
+			swagger.maximum = max.arg;
+		}
+
+		var valids = schema._valids.values().filter((s) => typeof s === 'number');
+		if (get(schema, '_flags.allowOnly') && valids.length) {
+			swagger.enum = valids;
+		}
+
+		return swagger;
+	},
+	string: (schema) => {
+		var swagger = { type: 'string' };
+
+		var strict = get(schema, '_settings.convert') === false;
+
+		if (find(schema._tests, { name: 'alphanum' })) {
+			if (strict && find(schema._tests, { name: 'lowercase' })) {
+				swagger.pattern = '/^[a-z0-9]*$/';
+			} else if (strict && find(schema._tests, { name: 'uppercase' })) {
+				swagger.pattern = '/^[A-Z0-9]*$/';
+			} else {
+				swagger.pattern = '/^[a-zA-Z0-9]*$/';
+			}
+		}
+
+		if (find(schema._tests, { name: 'token' })) {
+			if (find(schema._tests, { name: 'lowercase' })) {
+				swagger.pattern = '/^[a-z0-9_]*$/';
+			} else if (find(schema._tests, { name: 'uppercase' })) {
+				swagger.pattern = '/^[A-Z0-9_]*$/';
+			} else {
+				swagger.pattern = '/^[a-zA-Z0-9_]*$/';
+			}
+		}
+
+		if (find(schema._tests, { name: 'email' })) {
+			swagger.format = 'email';
+			if (swagger.pattern) delete swagger.pattern;
+		}
+
+		if (find(schema._tests, { name: 'isoDate' })) {
+			swagger.format = 'date-time';
+			if (swagger.pattern) delete swagger.pattern;
+		}
+
+		var pattern = find(schema._tests, { name: 'regex' });
+		if (pattern) {
+			swagger.pattern = pattern.arg.pattern.toString();
+		}
+
+		for (let i = 0; i < schema._tests.length; i++) {
+			const test = schema._tests[i];
+			if (test.name === 'min') {
+				swagger.minLength = test.arg;
+			}
+
+			if (test.name === 'max') {
+				swagger.maxLength = test.arg;
+			}
+
+			if (test.name === 'length') {
+				swagger.minLength = test.arg;
+				swagger.maxLength = test.arg;
+			}
+		}
+
+		var valids = schema._valids.values().filter((s) => typeof s === 'string');
+		if (get(schema, '_flags.allowOnly') && valids.length) {
+			swagger.enum = valids;
+		}
+
+		return swagger;
+	},
+	binary: (schema) => {
+		var swagger = { type: 'string', format: 'binary' };
+
+		if (get(schema, '_flags.encoding') === 'base64') {
+			swagger.format = 'byte';
+		}
+
+		for (let i = 0; i < schema._tests.length; i++) {
+			const test = schema._tests[i];
+			if (test.name === 'min') {
+				swagger.minLength = test.arg;
+			}
+
+			if (test.name === 'max') {
+				swagger.maxLength = test.arg;
+			}
+
+			if (test.name === 'length') {
+				swagger.minLength = test.arg;
+				swagger.maxLength = test.arg;
+			}
+		}
+
+		return swagger;
+	},
+	date: () => ({ type: 'string', format: 'date-time' }),
+	boolean: () => ({ type: 'boolean' }),
+	alternatives: (schema, existingDefinitions, newDefinitionsByRef) => {
+		var index = meta(schema, 'swaggerIndex') || 0;
+		var itemsSchema = get(schema, [ '_inner', 'matches', index, 'schema' ]);
+		var items = exports(itemsSchema, Object.assign({}, existingDefinitions || {}, newDefinitionsByRef || {}));
+
+		Object.assign(newDefinitionsByRef, items.definitions || {});
+
+		return items.swagger;
+	},
+	array: (schema, existingDefinitions, newDefinitionsByRef) => {
+		var index = meta(schema, 'swaggerIndex') || 0;
+		var itemsSchema = get(schema, [ '_inner', 'items', index ]);
+
+		if (!itemsSchema) throw Error('Array schema does not define an items schema at index ' + index);
+
+		var items = exports(itemsSchema, Object.assign({}, existingDefinitions || {}, newDefinitionsByRef || {}));
+
+		Object.assign(newDefinitionsByRef, items.definitions || {});
+
+		var swagger = { type: 'array' };
+
+		for (let i = 0; i < schema._tests.length; i++) {
+			const test = schema._tests[i];
+			if (test.name === 'min') {
+				swagger.minItems = test.arg;
+			}
+
+			if (test.name === 'max') {
+				swagger.maxItems = test.arg;
+			}
+
+			if (test.name === 'length') {
+				swagger.minItems = test.arg;
+				swagger.maxItems = test.arg;
+			}
+		}
+
+		if (find(schema._tests, { name: 'unique' })) {
+			swagger.uniqueItems = true;
+		}
+
+		swagger.items = items.swagger;
+		return swagger;
+	},
+	object: (schema, existingDefinitions, newDefinitionsByRef) => {
+
+		var requireds = [];
+		var properties = {};
+
+		var combinedDefinitions = Object.assign({}, existingDefinitions || {}, newDefinitionsByRef || {});
+
+		schema._inner.children.forEach((child) => {
+			var key = child.key;
+			var prop = exports(child.schema, combinedDefinitions);
+
+			Object.assign(newDefinitionsByRef, prop.definitions || {});
+			Object.assign(combinedDefinitions, prop.definitions || {});
+
+			properties[key] = prop.swagger;
+
+			if (get(child.schema, '_flags.presence') === 'required') {
+				requireds.push(key);
+			}
+		});
+
+		var swagger = { type: 'object' };
+		if (requireds.length) {
+			swagger.required = requireds;
+		}
+		swagger.properties = properties;
+
+		if (get(schema, '_flags.allowUnknown') === false) {
+			swagger.additionalProperties = false;
+		}
+
+		return swagger;
+	},
+};
+
+function meta (schema, key) {
+	var flattened = Object.assign.apply(null, [ {} ].concat(schema._meta));
+
+	return get(flattened, key);
+}
+
+function refDef (name) {
+	return { $ref: '#/definitions/' + name };
+}
+
+// var inspectU = require('util').inspect;
+// function inspect (value) {
+// 	console.error(inspectU(value, { colors: true, depth: 10 }));
+// }
---
+++ tests.js
@@ -0,0 +1,286 @@
+
+var suite = require('tapsuite');
+var parser = require('./');
+var joi = require('joi');
+
+suite('swagger converts', (s) => {
+	var i = 0;
+	function simpleTest (input, output, definitions, only) {
+		s[only ? 'only' : 'test']('Set ' + i++, (t) => {
+			var result = parser(input);
+			t.deepEqual(result.swagger, output, 'swagger matches');
+			if (definitions) t.deepEqual(result.definitions, definitions, 'definitions match');
+			t.end();
+		});
+	}
+
+	simpleTest(
+		joi.number().integer().min(1).max(10),
+		{
+			type: 'integer',
+			maximum: 10,
+			minimum: 1,
+		}
+	);
+
+	simpleTest(
+		joi.number().positive(),
+		{
+			type: 'number',
+			format: 'float',
+			minimum: 1,
+		}
+	);
+
+	simpleTest(
+		joi.number().precision(2).negative(),
+		{
+			type: 'number',
+			format: 'double',
+			maximum: -1,
+		}
+	);
+
+	simpleTest(
+		joi.string(),
+		{
+			type: 'string',
+		}
+	);
+
+	simpleTest(
+		joi.string().regex(/^A$/),
+		{
+			type: 'string',
+			pattern: '/^A$/',
+		}
+	);
+
+	simpleTest(
+		joi.string().min(4).max(9),
+		{
+			type: 'string',
+			maxLength: 9,
+			minLength: 4,
+		}
+	);
+
+	simpleTest(
+		joi.string().min(4).max(9).length(14),
+		{
+			type: 'string',
+			maxLength: 14,
+			minLength: 14,
+		}
+	);
+
+	simpleTest(
+		joi.string().max(9).length(14).min(4),
+		{
+			type: 'string',
+			maxLength: 14,
+			minLength: 4,
+		}
+	);
+
+	simpleTest(
+		joi.string().alphanum(),
+		{
+			type: 'string',
+			pattern: '/^[a-zA-Z0-9]*$/',
+		}
+	);
+
+	simpleTest(
+		joi.string().strict().alphanum().lowercase(),
+		{
+			type: 'string',
+			pattern: '/^[a-z0-9]*$/',
+		}
+	);
+
+	simpleTest(
+		// confirm that non-strict mode enables insensitive match
+		joi.string().alphanum().uppercase(),
+		{
+			type: 'string',
+			pattern: '/^[a-zA-Z0-9]*$/',
+		}
+	);
+
+	simpleTest(
+		joi.string().strict().alphanum().uppercase(),
+		{
+			type: 'string',
+			pattern: '/^[A-Z0-9]*$/',
+		}
+	);
+
+	simpleTest(
+		joi.string().alphanum().email(),
+		{
+			type: 'string',
+			format: 'email',
+		}
+	);
+
+	simpleTest(
+		joi.string().alphanum().isoDate(),
+		{
+			type: 'string',
+			format: 'date-time',
+		}
+	);
+
+	simpleTest(
+		joi.string().only('A', 'B', 'C', null),
+		{
+			type: [ 'string', 'null' ],
+			enum: [ 'A', 'B', 'C' ],
+		}
+	);
+
+	simpleTest(
+		joi.boolean(),
+		{
+			type: 'boolean',
+		}
+	);
+
+	simpleTest(
+		joi.boolean().allow(null),
+		{
+			type: [ 'boolean', 'null' ],
+		}
+	);
+
+	simpleTest(
+		joi.binary(),
+		{
+			type: 'string',
+			format: 'binary',
+		}
+	);
+
+	simpleTest(
+		joi.binary().encoding('base64'),
+		{
+			type: 'string',
+			format: 'byte',
+		}
+	);
+
+	simpleTest(
+		joi.array().items(joi.boolean(), joi.date()),
+		{
+			type: 'array',
+			items: { type: 'boolean' },
+		}
+	);
+
+	simpleTest(
+		joi.array().items(joi.string()).unique(),
+		{
+			type: 'array',
+			uniqueItems: true,
+			items: { type: 'string' },
+		}
+	);
+
+	simpleTest(
+		joi.array().items(joi.string(), joi.number()).meta({ swaggerIndex: 1 }).min(1).max(5),
+		{
+			type: 'array',
+			items: { type: 'number', format: 'float' },
+			minItems: 1,
+			maxItems: 5,
+		}
+	);
+
+	simpleTest(
+		joi.alternatives(joi.string(), joi.number()).meta({ swaggerIndex: 1 }),
+		{ type: 'number', format: 'float' }
+	);
+
+	simpleTest(
+		joi.object().keys({
+			id: joi.number().integer().required(),
+			name: joi.string(),
+		}),
+		{
+			type: 'object',
+			required: [ 'id' ],
+			properties: {
+				id: { type: 'integer' },
+				name: { type: 'string' },
+			},
+		}
+	);
+
+	simpleTest(
+		joi.object().keys({
+			value: joi.string().default('hello'),
+		}).unknown(false),
+		{
+			type: 'object',
+			additionalProperties: false,
+			properties: {
+				value: { type: 'string', default: 'hello' },
+			},
+		}
+	);
+
+	simpleTest(
+		joi.string().alphanum().email().meta({ className: 'Email' }),
+		{
+			$ref: '#/definitions/Email',
+		},
+		{
+			Email: {
+				type: 'string',
+				format: 'email',
+			},
+		}
+	);
+
+	simpleTest(
+		{
+			start: joi.object().unknown(false).keys({
+				lat:  joi.number().min(-90).max(90).required(),
+				lon:  joi.number().min(-180).max(180).required(),
+			}).meta({ className: 'GeoPoint' }),
+			stop: joi.object().unknown(false).keys({
+				lat:  joi.number().min(-90).max(90).required(),
+				lon:  joi.number().min(-180).max(180).required(),
+			}).meta({ className: 'GeoPoint' }),
+		},
+		{
+			type: 'object',
+			properties: {
+				start: { $ref: '#/definitions/GeoPoint' },
+				stop: { $ref: '#/definitions/GeoPoint' },
+			},
+		},
+		{
+			GeoPoint: {
+				type: 'object',
+				required: [ 'lat', 'lon' ],
+				additionalProperties: false,
+				properties: {
+					lat: {
+						type: 'number',
+						format: 'float',
+						minimum: -90,
+						maximum: 90,
+					},
+					lon: {
+						type: 'number',
+						format: 'float',
+						minimum: -180,
+						maximum: 180,
+					},
+				},
+			},
+		}
+	);
+});
