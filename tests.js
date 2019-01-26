33c8f6102c0e6382cbf979a65fba5a81ae207413
wangqiang <1432613064@qq.com>
2019-01-26 17:00:42 +0800

    file support

--- tests.js
+++ tests.js
@@ -434,4 +434,14 @@ suite('swagger converts', (s) => {
 			format: 'date-time',
 		}
 	);
+
+	// test files
+	simpleTest(
+		joi.any().meta({ swaggerType: 'file' }).description('simpleFile'),
+		{
+			description: 'simpleFile',
+			in: 'formData',
+			type: 'file',
+		}
+	);
 });
